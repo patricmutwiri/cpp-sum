ceb1486ebc2b0b562fdefa3c509a5bf2a37f07b7
Patrick Mutwiri <patwiri@gmail.com>
2020-02-06 18:53:29 +0300

    sum test :)

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x,y,sum;
+    cout << "Enter y ";
+    cin>>x;
+    cout << "Enter x ";
+    cin>>y;
+
+    sum = x+y;
+
+    cout<<"The sum of "<<x<<" and "<<y<<" is ";
+    cout<<sum<<"\n";
+
+    return 0;
+}
